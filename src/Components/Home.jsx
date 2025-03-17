64cf943c93bb4dc7943d65d1ae24440a64644909
Muhammad Owais <150528409+MalikOwaisDev@users.noreply.github.com>
2025-03-17 18:05:24 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   const getProductsCategory = async () => {
     try {
-      const { data } = await axios.get(`/products/category/${category}`);
+      const { data } = await axios.get(`/testproducts/products/category/${category}`);
       setfilteredProducts(data);
     } catch (error) {
       console.log(error);
@@ -34,7 +34,7 @@ const Home = () => {
             return (
               <Link
                 key={i}
-                to={`/details/${p.id}`}
+                to={`/testproducts/details/${p.id}`}
                 className="card p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center"
               >
                 <div
